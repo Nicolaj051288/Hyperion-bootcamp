c39ebbb3122b9fa34d498802bebea1d7cecfe7d7
Nicolaj Strøm Vasudevan <pedersen_22@hotmail.com>
2025-01-02 16:42:45 +0100

    Update pattern.py

--- pattern.py
+++ pattern.py
@@ -1,20 +1,7 @@
-star = "*"
-for i in range(0,9):
-    if i == 0:
-        print((star) * 1)
-    elif i == 1:
-        print((star) * 2)
-    elif i == 2:
-        print((star) * 3)
-    elif i == 3:
-        print((star) * 4)
-    elif i == 4:
-        print((star) * 5)
-    elif i == 5:
-        print((star) * 4)
-    elif i == 6:
-        print((star) * 3)
-    elif i == 7:
-        print((star) * 2)
-    elif i == 8:
-        print((star) * 1)
+rows = 9 #indexes the number of rows needed
+for i in range(rows):
+    if i < 5: #calculates i until i==5
+        print("*" * (i+1))
+    else: # calculates i above 5 
+        i = rows - i 
+        print("*" * i)
